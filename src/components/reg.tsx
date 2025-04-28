ed6e76a28693f25dc094217e0ef29d4de8e66c30
Peppetesta <sonsycomb@gmail.com>
2025-04-28 22:04:25 +0200

    Fix del package.json

--- src/components/reg.tsx
+++ src/components/reg.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import '../styles/style.css';
 import Script from 'next/script';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Reg() {
   const router = useRouter();
@@ -45,70 +46,38 @@ export default function Reg() {
 
   return (
     <div className="login-container">
-      <Script src="/scripts/registrazione.js"></Script>
-
-      {/* Sinistra */}
+      <Script src="/scripts/login.js"></Script>
       <div className="left">
-        <img src="/img/logo.png" alt="Logo" />
+        <img src="/img/logo.png" alt="Logo"/> 
         <h1>DietiEstates25</h1>
         <p>Dove l’arte incontra l’immobiliare</p>
         <div className="credits">
           Designed by<br /><b>STICY Tech.</b>
         </div>
       </div>
-
-      {/* Destra */}
       <div className="right">
-        <h2>Registrati</h2>
-
-        {/* Input per username, email e password */}
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <h2>Sign in</h2>
+        <button className="btn">
+          <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google" />
+          Continue with Google
+        </button>
+        <button className="btn">
+          <img src="https://img.icons8.com/color/48/000000/facebook-new.png" alt="Facebook" />
+          Continue with Facebook
+        </button>
+        <div className="divder">OR</div>
+        <input id="username" type="text" placeholder="User name or email address" />
         <button className='hide'>👁️‍🗨️</button>
-        <input
-          type="password"
-          placeholder="Your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        {/* Scelta Cliente / Agente */}
-        <div className="ruolo-selection">
-          <p>Registrati come:</p>
-          <div className="ruolo-buttons">
-            <button
-              type="button"
-              onClick={() => setRuolo('cliente')}
-              className={`ruolo-btn ${ruolo === 'cliente' ? 'selected' : ''}`}
-            >
-              Cliente
-            </button>
-            <button
-              type="button"
-              onClick={() => setRuolo('agente')}
-              className={`ruolo-btn ${ruolo === 'agente' ? 'selected' : ''}`}
-            >
-              Agente Immobiliare
-            </button>
-          </div>
+        <input id="password" type="password" placeholder="Your password"/>
+        <div className="password-options">
+          <a href="#">Forgot your password?</a>
         </div>
-
-        <button onClick={handleSignup} className="signin-btn">Registrati</button>
-
+        <button id="login" className="signin-btn">Sign in</button>
         <div className="signup">
-          Hai già un account? <a href="/start">Accedi</a>
+          Don’t have an account? <Link href="/registrazione">Sign up</Link>
         </div>
       </div>
     </div>
   );
 }
+
